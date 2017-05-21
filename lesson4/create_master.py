db255e1d23506f36707bf111d905c66c8efda6cf
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-05-21 08:18:10 +0800

    4.6 master file prepared

--- lesson4/create_master.py
+++ lesson4/create_master.py
@@ -26,7 +26,10 @@ def create_master_turnstile_file(filenames, output_file):
     with open(output_file, 'w') as master_file:
         master_file.write('C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn\n')
         for filename in filenames:
-            print(filename)
+            with open(filename, 'r') as source:
+                for line in source.readlines():
+                    master_file.write(line)
+
 
 
 if __name__ == '__main__':
