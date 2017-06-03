4575cea29a45bf9ac5bcc8aee394c78679d79e85
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-06-03 06:20:47 +0300

    7.27 comparing two teams using ggplot

--- lesson7_visualization/lineplot_compare.py
+++ lesson7_visualization/lineplot_compare.py
@@ -2,7 +2,8 @@ from pandas import *
 from ggplot import *
 
 def lineplot_compare():
-    pass
+    df = pandas.read_csv('hr_by_team_year_sf_la.csv')
+    print(ggplot(df, aes(x='yearID',y='HR', color='teamID')) + geom_line())
 
 
 if __name__ == '__main__':
